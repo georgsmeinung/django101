d395be98ac167c2924eb5d49892a4e6cfa043da7
georgsmeinung <jenicolau@gmail.com>
2020-10-15 15:11:11 -0300

    Added Edit Form

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.show_post, name='show_post'),
     path('post/new/', views.create_post, name='create_post'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
