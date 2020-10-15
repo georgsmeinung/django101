7417a14552d68e533611e266c5d28b5c1ad4d6ce
georgsmeinung <jenicolau@gmail.com>
2020-10-15 14:49:10 -0300

    Added Form

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.show_post, name='show_post'),
+    path('post/new/', views.create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 def post_list(request):
@@ -9,3 +10,16 @@ def post_list(request):
 def show_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/show_post.html', {'post': post})
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('show_post', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
